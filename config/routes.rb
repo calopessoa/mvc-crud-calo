ee6275ce712c89e3e3619a7ad6a826904bb41e3c
Carlos <nashlasterby@gmail.com>
2022-11-25 10:15:05 -0300

    feat: created usuarios as alternative for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
+  # Alternative route for users
+  get 'usuarios', to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
