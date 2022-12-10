bbf6af1fb7132662e8d12b060e23e917c29f1075
Carlos <nashlasterby@gmail.com>
2022-12-10 17:41:52 -0300

    feat: uniqueness for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    render :template => "/users/error", :status => 404
+  end
 end
